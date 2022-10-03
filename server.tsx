8bfe44cfca688cff9d87ce4491309a445d9df65f
Monika <monidavies@gmail.com>
2022-10-02 19:41:04 -0700

    adds server.tsx

---
+++ server.tsx
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+require('dotenv').config();
+
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
