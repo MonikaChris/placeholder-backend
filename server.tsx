0c32ff8ba0c863b87c84ae6faea9458bed5d19af
Monika <monidavies@gmail.com>
2022-10-16 20:39:01 -0700

    Adds getPhotos route

--- server.tsx
+++ server.tsx
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const data = require('./data.json');
 
 
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/photos', (req, res) => {
+  res.send(data);
+
 });
 
 app.listen(port, () => {
